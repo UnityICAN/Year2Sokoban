4107c66ef5af582d48836328663d14ccb023fbab
Lenophie <lenophie.dev@gmail.com>
2023-10-19 12:03:18 +0200

    Prevent movements through walls

--- Assets/BoardManager.cs
+++ Assets/BoardManager.cs
@@ -8,21 +8,23 @@ public class BoardManager : MonoBehaviour {
     [SerializeField] private Sprite wallSprite;
     [SerializeField] private Sprite floorSprite;
 
-    private TileType[,] tilesList;
+    public TileType[,] TilesList { get; private set; }
     
     private void Start() {
-        playerController.Init();
+        playerController.Init(this);
         
         // Créer le niveau
-        tilesList = new TileType[10, 10];
+        TilesList = new TileType[10, 10];
         for (int x = 0; x < 10; x++) {
             for (int y = 0; y < 10; y++) {
                 if (x == 0 || y == 0 || x == 9 || y == 9)
-                    tilesList[x, y] = TileType.Wall;
+                    TilesList[x, y] = TileType.Wall;
                 else
-                    tilesList[x, y] = TileType.Floor;
+                    TilesList[x, y] = TileType.Floor;
             }
         }
+
+        TilesList[3, 2] = TileType.Wall;
 
         // Afficher le niveau
         for (int x = 0; x < 10; x++) {
@@ -31,9 +33,9 @@ public class BoardManager : MonoBehaviour {
                     new Vector3(x, -y, 0f),
                     Quaternion.identity,
                     boardTransform);
-                if (tilesList[x,y] == TileType.Wall)
+                if (TilesList[x,y] == TileType.Wall)
                     tile.GetComponent<SpriteRenderer>().sprite = wallSprite;
-                else if (tilesList[x,y] == TileType.Floor)
+                else if (TilesList[x,y] == TileType.Floor)
                     tile.GetComponent<SpriteRenderer>().sprite = floorSprite;
             }
         }
--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -3,31 +3,41 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
     [SerializeField] private Transform playerTransform;
     private Vector2Int position;
+    private BoardManager boardManager;
     
-    public void Init() {
-        position = new Vector2Int(4, -4);
+    public void Init(BoardManager boardManager) {
+        this.boardManager = boardManager;
+        position = new Vector2Int(4, 4);
         MakeVisualMovement();
     }
     
     private void MakeVisualMovement() {
-        playerTransform.position = new Vector3(position.x, position.y);
+        playerTransform.position = new Vector3(position.x, -position.y);
     }
     
     private void Update() {
-        if (Input.GetButtonUp("Horizontal")) {
+        Vector2Int projectedPosition = position;
+        
+        if (Input.GetButtonDown("Horizontal")) {
             float x = Input.GetAxis("Horizontal");
             if (x > 0f)
-                position.x += 1;
+                projectedPosition.x += 1;
             else if (x < 0f)
-                position.x -= 1;
-        } else if (Input.GetButtonUp("Vertical")) {
+                projectedPosition.x -= 1;
+        } else if (Input.GetButtonDown("Vertical")) {
             float y = Input.GetAxis("Vertical");
             if (y > 0f)
-                position.y += 1;
+                projectedPosition.y -= 1;
             else if (y < 0f)
-                position.y -= 1;
+                projectedPosition.y += 1;
         }
-        
-        MakeVisualMovement();
+
+        if (boardManager.TilesList[projectedPosition.x, projectedPosition.y] == TileType.Wall)
+            projectedPosition = position;
+
+        if (projectedPosition != position) {
+            position = projectedPosition;
+            MakeVisualMovement();
+        }
     }
 }
