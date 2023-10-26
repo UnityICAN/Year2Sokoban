2280dda7e44b7b6623848df8f07fde95cd5bad59
Lenophie <lenophie.dev@gmail.com>
2023-10-26 12:09:03 +0200

    Add LevelLoader window

---
+++ Assets/Editor/LevelLoader.cs
@@ -0,0 +1,20 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Editor {
+    public class LevelLoader : EditorWindow {
+        private Level selectedLevel;
+        
+        [MenuItem("Window/Level Loader")]
+        public static void OpenWindow() {
+            LevelLoader window = GetWindow<LevelLoader>();
+            window.titleContent = new GUIContent("Level Loader");
+        }
+
+        private void OnGUI() {
+            selectedLevel = (Level)EditorGUILayout.ObjectField(selectedLevel, typeof(Level));
+            if (GUILayout.Button("Load") && selectedLevel != null)
+                Debug.Log("Load!");
+        }
+    }
+}
